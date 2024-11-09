20dd60418ad71a3549b6e93fc16aa5a07a2f9b8e
vighnesh-kargutkar <kargutkar860@gmail.com>
2024-11-09 12:22:45 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
+import Header from "./components/header";
+import UserInput from "./components/UserInput";
+import Result from "./components/Result";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prev) => {
+      return {
+        ...prev,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      <Result input={userInput} />
+    </>
+  );
 }
 
-export default App
+export default App;
